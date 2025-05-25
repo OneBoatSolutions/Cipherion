e1e577cdfc9846c99c3214be87b377b32db46bb7
OneBoatSolutions <onebotesolution@gmail.com>
2025-05-25 14:03:25 +0530

    build fixes

--- src/components/ui/globe.tsx
+++ src/components/ui/globe.tsx
@@ -64,7 +64,8 @@ interface WorldProps {
 
 export function Globe({ globeConfig, data }: WorldProps) {
   const globeRef = useRef<ThreeGlobe | null>(null);
-  const groupRef = useRef();
+  const groupRef = useRef<THREE.Group>(null!);
+
   const [isInitialized, setIsInitialized] = useState(false);
 
   const defaultProps = {
@@ -87,7 +88,8 @@ export function Globe({ globeConfig, data }: WorldProps) {
   useEffect(() => {
     if (!globeRef.current && groupRef.current) {
       globeRef.current = new ThreeGlobe();
-      (groupRef.current as any).add(globeRef.current);
+      groupRef.current.add(globeRef.current!);
+
       setIsInitialized(true);
     }
   }, []);
@@ -164,7 +166,7 @@ export function Globe({ globeConfig, data }: WorldProps) {
       .arcStartLng((d) => (d as { startLng: number }).startLng * 1)
       .arcEndLat((d) => (d as { endLat: number }).endLat * 1)
       .arcEndLng((d) => (d as { endLng: number }).endLng * 1)
-      .arcColor((e: any) => (e as { color: string }).color)
+      .arcColor((e: Position) => e.color)
       .arcAltitude((e) => (e as { arcAlt: number }).arcAlt * 1)
       .arcStroke(() => [0.32, 0.28, 0.3][Math.round(Math.random() * 2)])
       .arcDashLength(defaultProps.arcLength)
